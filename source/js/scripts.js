048f3182cf5d9b63ee2ce1fe9107f5aad4ca9504
Дмитрий Метельков <d.metelkov@mail.ru>
2018-05-08 17:40:06 +0400

    Сделал меню открытым, при отключенном js

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,6 +1,8 @@
 var hamburgerBtn = document.querySelector(".hamburger-btn");
 var pageHeaderNav = document.querySelector(".page-header__nav");
 
+pageHeaderNav.classList.remove("page-header__nav--show");
+
 hamburgerBtn.addEventListener("click", function () {
   pageHeaderNav.classList.toggle("page-header__nav--show");
   hamburgerBtn.classList.toggle("hamburger-btn--close");
